f2184b25b66303e6e5fb5b2803855bddb4ae0bba
fvmill <33756642+fvmill@users.noreply.github.com>
2017-11-17 10:31:11 -0500

    Second Upload

--- madlib_FM.py
+++ madlib_FM.py
@@ -1,3 +1,5 @@
+import time
+
 ### Mad Libs ###
 ### Source: http://ordinaryhumanlanguage.ca/blog/wp-content/uploads/2017/07/Mad-Libs-Example.jpg
 
@@ -57,6 +59,8 @@ pob = input()
 ###Begin Mad Lib Here###
 
 print("It was Thanksgiving, and the scent of succulent roast " + noun1 + " wafted through my house.")
-print("'" + person + ", it's time to" + verb1 + "!'" "my mother called.")
-print("I couldn't wait to get my" + pob1 + "on that" + adj2 + "Thanksgiving meal.")
-print("My family sat around the dining-room" + noun2 +
+print("'" + person + ", it's time to " + verb1 + "!'" "my mother called.")
+print("I couldn't wait to get my " + pob1 + " on that " + adj2 + " Thanksgiving meal.")
+print("My family sat around the dining-room" + noun2 + ". The table was laid out with every kind of" + noun3 + " imaginable.")
+print("There was a basket of hot buttered" + pnoun1 + "and glasses of sparkling" + liquid1+ ".")
+print
